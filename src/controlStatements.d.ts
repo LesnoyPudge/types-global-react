5684612093a258d344644be695c1a4e6b6e60b46
LesnoyPudge <chuykin.a01@gmail.com>
2024-11-24 17:10:40 +0500

    'update'

--- src/controlStatements.d.ts
+++ src/controlStatements.d.ts
@@ -1,9 +1,33 @@
-declare function If(props: { 
-    condition: unknown;
-    children: React.ReactNode;
+import type { 
+    RT 
+} from "@lesnoypudge/types-utils-react/namespace";
+
+
+
+declare function If(props: (
+    RT.PropsWithRequiredChildren &
+    {
+        condition: boolean;
+    }
+)): React.ReactNode;
+
+// https://github.com/AlexGilleran/jsx-control-statements?tab=readme-ov-file#for-tag---alternative-syntax
+declare function For<T>(props: {
+    of: T[];                        
+    body: (item: T, index: number, arr: T[]) => React.ReactNode;
 }): React.ReactNode;
 
-declare function For<T>(props: {
-    of: Iterable<T>;
-    body: (item: T, index?: number) => React.ReactNode;
-}): React.ReactNode;
+declare function Choose(
+    props: RT.PropsWithRequiredChildren
+): React.ReactNode;
+
+declare function When(props: (
+    RT.PropsWithRequiredChildren &
+    {
+        condition: boolean;
+    }
+)): React.ReactNode;
+
+declare function Otherwise(props: (
+    RT.PropsWithRequiredChildren
+)): React.ReactNode;
