16bf39b2dac7308e9987b0d702002d20272ae9ed
LesnoyPudge <chuykin.a01@gmail.com>
2024-12-10 07:22:49 +0500

    'update'

--- src/controlStatements.d.ts
+++ src/controlStatements.d.ts
@@ -7,20 +7,20 @@ declare function If(props: {
 }): React.ReactNode;
 
 // https://github.com/AlexGilleran/jsx-control-statements?tab=readme-ov-file#for-tag---alternative-syntax
-declare function For<T>(props: {
-    of: T[];                        
-    body: (item: T, index: number, arr: T[]) => React.ReactNode;
-}): React.ReactNode;
+// declare function For<T>(props: {
+//     of: T[];                        
+//     body: (item: T, index: number, arr: T[]) => React.ReactNode;
+// }): React.ReactNode;
 
-declare function Choose(props: {
-    children: React.ReactNode;
-}): React.ReactNode;
+// declare function Choose(props: {
+//     children: React.ReactNode;
+// }): React.ReactNode;
 
-declare function When(props: {
-    children: React.ReactNode;
-    condition: boolean;
-}): React.ReactNode;
+// declare function When(props: {
+//     children: React.ReactNode;
+//     condition: boolean;
+// }): React.ReactNode;
 
-declare function Otherwise(props: {
-    children: React.ReactNode;
-}): React.ReactNode;
+// declare function Otherwise(props: {
+//     children: React.ReactNode;
+// }): React.ReactNode;
