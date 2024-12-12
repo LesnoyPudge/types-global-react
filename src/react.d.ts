94b1fba5bf3c1fc7494c20a21f55d1d0ef8d3d45
LesnoyPudge <chuykin.a01@gmail.com>
2024-12-13 02:30:43 +0500

    'update'

--- src/react.d.ts
+++ src/react.d.ts
@@ -2,14 +2,8 @@ import R from "react";
 
 
 
-declare namespace React {
-    interface RefObject<T> {
-        current: T | null;
-    }
-}
-
 declare module "react" {
     function createContext<T>(defaultValue?: T): R.Context<T>;
 
-    function useRef<T>(initialValue: T | null): React.RefObject<T>;
+    function useRef<T = null>(initialValue: null): R.MutableRefObject<T | null>;
 }
