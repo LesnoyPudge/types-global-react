1788c84f74e06e2163da4c53fa8c086fefd166cc
LesnoyPudge <chuykin.a01@gmail.com>
2025-01-27 23:29:09 +0500

    'update'

--- src/_index.d.ts
+++
@@ -1,2 +0,0 @@
-import './react.d.ts'
-import './controlStatements.d.ts'
---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+import '@types/react';
+import '@types/react-dom';
+import './react.d.ts';
+import './controlStatements.d.ts';
